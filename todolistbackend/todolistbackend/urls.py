2de15a80337fce99f34745ecb3da6b5e29a3272b
Alen Alduk <youstylez@gmail.com>
2023-11-26 11:52:38 +0100

    added auth token

--- todolistbackend/todolistbackend/urls.py
+++ todolistbackend/todolistbackend/urls.py
@@ -17,6 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from todolist.views import LoginView
+
+urlpatterns = [path("admin/", admin.site.urls), path("login/", LoginView.as_view())]
